18dd457eb5a363135bc46f9d47895042337c299b
Mr. Macfadden <mmacfadden@glenbrook225.org>
2021-12-08 10:34:05 -0600

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function show(){
+    document.getElementById("panel-1").style.display = "none";
+    document.getElementById("panel-2").style.display = "block";
+}
+
+function hide(){
+    document.getElementById("panel-1").style.display = "block";
+    document.getElementById("panel-2").style.display = "none";
+}
