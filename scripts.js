ce5883565d7795509061230702f348250c78ec46
Mike Macfadden <mikemacfadden@gmail.com>
2021-12-08 11:31:03 -0600

    Dec. 8

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
-function show(){
-    document.getElementById("panel-1").style.display = "none";
+function showPanel1(){
+    document.getElementById("panel-1").style.display = "block";
+}
+function showPanel2(){
     document.getElementById("panel-2").style.display = "block";
 }
+function showPanel3(){
+    document.getElementById("panel-3").style.display = "block";
+}
+function showPanel4(){
+    document.getElementById("panel-4").style.display = "block";
+}
+
 
 function hide(){
-    document.getElementsByClassName('panel').style.display = "none";
+    panels=document.getElementsByClassName("panel");  // Find the elements
+        for(var i = 0; i < panels.length; i++){
+            panels[i].style.display = "none";    // Change the content
+    }
+
 }
